2de467990d4526add7d6820bd98df1ee959d7f69
Raphael Gava <raphaelgava1@gmail.com>
2025-05-13 00:27:01 -0300

    Example 4 - Microservice with actuator

--- src/main/java/com/example/microservice/MicroserviceApplication.java
+++ src/main/java/com/example/microservice/MicroserviceApplication.java
@@ -2,10 +2,8 @@ package com.example.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
 public class MicroserviceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/microservice/exemplo4/configurations/GreetingConfiguration.java
+++ src/main/java/com/example/microservice/exemplo4/configurations/GreetingConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.microservice.exemplo4.configurations;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope //Para fazer atualização dinâmica das configurações pelo http://localhost:8080/actuator/refresh sem reset ta aplicação
 @ConfigurationProperties ("greeting-service")
 public class GreetingConfiguration {
     private String greeting;
